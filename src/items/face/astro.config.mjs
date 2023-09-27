b110dccd9fef54b38022b3a39dcc9c019eff27a1
Sinbad <1228067812@qq.com>
2023-09-27 19:31:38 +0800

    update

--- src/items/face/astro.config.mjs
+++ src/items/face/astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+const [, item] = process.env.npm_lifecycle_event.split(':')
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://SinbadCQ.github.io',
-  base: '/my-astro-admin'
+  // base: '/my-astro-admin',
+  // root: `./src/items/${item}`,
+  // srcDir: `./src/items/${item}`,
+  publicDir: `./public`,
 });
---
+++ src/items/face/src/components/Login/index.jsx
@@ -0,0 +1,7 @@
+
+export default function Login() {
+
+  // console.log(import.meta.env.PUBLIC_POKEAPI)
+
+  return <>123123123123</>;
+}
